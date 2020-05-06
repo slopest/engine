5d48733392c62badf7406f3b5b1d895a7c8b44b2
Théo Vidal <exybore@becauseofprog.fr>
2020-05-06 20:29:29 +0200

    feat: optimize lexer
    - Removed repetitive instruction read (code & block)
    - Optimized line parsing code
    - Add constants to specific characters
    - Allow blank lines and whitespaces
    - Add more error handling and documentation on these errors

--- slope.js
+++ slope.js
@@ -14,6 +14,11 @@ const SLOPE_INSTRUCTIONS = [
   'Rotation'
 ]
 
+const BLANK = ''
+const COMMENT = 'comment'
+const BLOCK_START = '('
+const BLOCK_END = ')'
+
 const INSTRUCTIONS = [
   {
     name: 'Name',
@@ -34,84 +39,81 @@ const INSTRUCTIONS = [
 ]
 
 class Lexer {
-  instructions
-  line
-  lines
-
-  croak(message) {
-    throw new Error(`Error in parsing Slope code at line ${this.line + 1} : ${message}`)
-  }
-
-  parseCode(code) {
-    this.line = 0
+  readCode(code) {
+    this.line = -1
     this.instructions = []
     this.lines = code.split('\n')
     
-    while (this.line < this.lines.length) {
-      console.log(this.instructions)
-      let line = this.lines[this.line].trim()
-      let instruction = this.parseInstruction(line)
-      if (instruction !== null) this.instructions.push(instruction)
-      this.line++
-    }
-
-    return this.instructions
+    return this.readProgram()
   }
 
-  parseInstruction(line) {
-    let params = line.split(' ')
-    if (params[0].charAt(0) === '#') {
-      return null
+  readProgram(inBlock = false) {
+    let instructions = []
+    while (true) {
+      let line = this.nextLine()
+      if (this.line === this.lines.length) {
+        if (inBlock) this.croak('Reached end of file without closing block. Review all the blocks you created and check if you closed them using the ")" statement.')
+        else break
+      }
+
+      line = line.trim()
+      if (inBlock && line === BLOCK_END) break
+      if (line === BLANK) continue
+
+      let instruction = this.readInstruction(line)
+      if (instruction !== COMMENT) instructions.push(instruction)
     }
+    return instructions
+  }
+
+  readInstruction(line) {
+    let params = line.split(' ')
+    if (params[0].charAt(0) === '#') return COMMENT
 
     let instruction = INSTRUCTIONS.find(i => i.name === params[0])
-    if (instruction === undefined) this.croak(`Unknown instruction ${params[0]}`)
+    if (instruction === undefined) this.croak(`Unknown instruction ${params[0]}. Check the documentation for a complete list of instructions.`)
     else {
+      let type = params[0].toLowerCase()
+      params.shift()
+
       switch (instruction.type) {
-        case 'string':
-          return this.parseStringInstruction(params)
-        case 'params':
-          return this.parseParamsInstruction(params)
-        case 'block':
-          return this.parseBlockInstruction(params)
+        case 'string': return this.readStringInstruction(type, params)
+        case 'params': return this.readParamsInstruction(type, params)
+        case 'block': return this.readBlockInstruction(type, params)
       }
     }
   }
 
-  parseStringInstruction(params) {
+  readStringInstruction(type, args) {
     return {
-      type: params[0],
-      value: params.slice(1).join(' ')
+      type,
+      value: args.join(' ')
     }
   }
 
-  parseParamsInstruction(params) {
+  readParamsInstruction(type, args) {
     return {
-      type: params[0],
-      params: params.slice(1)
+      type,
+      params: args
     }
   }
 
-  parseBlockInstruction(params) {
-    let head = {
-      type: params[0],
-      params: params.slice(1, -1)
+  readBlockInstruction(type, args) {
+    let start = args.pop()
+    if (start !== BLOCK_START) this.croak('Excepted a block start with the "(" character')
+    return {
+      type,
+      params: args,
+      instructions: this.readProgram(true)
     }
-    let instructions = []
-    while (true) {
-      this.line++
-      let line = this.lines[this.line]
-      if (line === undefined)
-        this.croak('Reached end of file without closing block')
+  }
 
-      line = line.trim()
-      if (line === ')')
-        break
-      
-      let toAdd = this.parseInstruction(line)
-      if (toAdd !== null) instructions.push(toAdd)
-    }
-    head.instructions = instructions
-    return head
+  nextLine() {
+    this.line++
+    return this.lines[this.line]
+  }
+
+  croak(message) {
+    throw new Error(`Error in parsing Slope code at line ${this.line + 1} : ${message}`)
   }
 }
