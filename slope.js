078345a9c224ae2b61e7cb287630aa827e4d200b
Théo Vidal <exybore@becauseofprog.fr>
2020-05-06 22:04:11 +0200

    feat: add dependency notion
    Added the notion of dependency for instructions that need to be wrapped inside blocks.

--- slope.js
+++ slope.js
@@ -13,9 +13,9 @@ const INSTRUCTIONS = [
   { name: 'Grid', type: 'params' },
 
   { name: 'Hold', type: 'block' },
-  { name: 'Pos', type: 'params' },
-  { name: 'Scale', type: 'params' },
-  { name: 'Rotation', type: 'params' }
+  { name: 'Pos', type: 'params', depends: 'Hold' },
+  { name: 'Scale', type: 'params', depends: 'Hold' },
+  { name: 'Rotation', type: 'params', depends: 'Hold' }
 ]
 
 class Lexer {
@@ -27,8 +27,9 @@ class Lexer {
     return this.readProgram()
   }
 
-  readProgram(inBlock = false) {
+  readProgram(block = null) {
     let instructions = []
+    let inBlock = block !== null
     while (true) {
       let line = this.nextLine()
       if (this.line === this.lines.length) {
@@ -40,27 +41,30 @@ class Lexer {
       if (inBlock && line === BLOCK_END) break
       if (line === BLANK) continue
 
-      let instruction = this.readInstruction(line)
+      let instruction = this.readInstruction(line, block)
       if (instruction !== COMMENT) instructions.push(instruction)
     }
     return instructions
   }
 
-  readInstruction(line) {
+  readInstruction(line, block) {
     let params = line.split(' ')
     if (params[0].charAt(0) === '#') return COMMENT
 
     let instruction = INSTRUCTIONS.find(i => i.name === params[0])
-    if (instruction === undefined) this.croak(`Unknown instruction ${params[0]}. Check the documentation for a complete list of instructions.`)
-    else {
-      let type = params[0].toLowerCase()
-      params.shift()
+    if (instruction === undefined)
+    this.croak(`Unknown instruction ${params[0]}. Check the documentation for a complete list of instructions.`)
 
-      switch (instruction.type) {
-        case 'string': return this.readStringInstruction(type, params)
-        case 'params': return this.readParamsInstruction(type, params)
-        case 'block': return this.readBlockInstruction(type, params)
-      }
+    let type = params[0].toLowerCase()
+    params.shift()
+
+    if (instruction.depends !== undefined && instruction.depends.toLowerCase() !== block)
+    this.croak(`Instruction ${instruction.name} depends on a ${instruction.depends} block. Try wrap it inside this block.`)
+
+    switch (instruction.type) {
+      case 'string': return this.readStringInstruction(type, params)
+      case 'params': return this.readParamsInstruction(type, params)
+      case 'block': return this.readBlockInstruction(type, params)
     }
   }
 
@@ -84,7 +88,7 @@ class Lexer {
     return {
       type,
       params: args,
-      instructions: this.readProgram(true)
+      instructions: this.readProgram(type)
     }
   }
 
@@ -97,3 +101,17 @@ class Lexer {
     throw new Error(`Error in parsing Slope code at line ${this.line + 1} : ${message}`)
   }
 }
+
+let lex = new Lexer()
+console.log(lex.readCode(
+`Name gros delire
+#Ceci est un commentaire
+Grid 5 14
+
+Hold yep (
+  Pos 4 8 9
+)
+
+Pos 4 8 9
+
+Name mdr`))
